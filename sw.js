92a34305c2fb28b9bf8090bc61ae1edcd9eb5f30
MisterTok <piecassa35@gmail.com>
2026-09-12 21:21:19 +0200

    Increment version from v6 to v7

--- sw.js
+++ sw.js
@@ -13,7 +13,7 @@
    reponse fausserait le jeu. Seules les IMAGES d'articles sont conservees,
    pour que la collection reste illustree hors ligne. */
 
-var VERSION = 'wikigacha-v6';   /* a incrementer quand la liste change */
+var VERSION = 'wikigacha-v7';   /* a incrementer quand la liste change */
 var COQUILLE = VERSION + '-coquille';   /* le jeu lui-meme */
 var POLICES  = VERSION + '-polices';
 var IMAGES   = 'wikigacha-images';      /* survit aux versions : c'est lourd */
